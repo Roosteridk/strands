2bc5e05c50cd514814279e9460fa3d70704ac573
LastS3cond <jaydon.n.bingham@gmail.com>
2024-06-02 23:37:21 -0400

    methodified

--- strands.py
+++ strands.py
@@ -1,5 +1,30 @@
 import networkx as nx
 
+def create_word_graph(matrix_to_board):
+
+    board_matrix = []
+
+    for row in starting_board:
+        board_matrix.append([row])
+
+    for lst in board_matrix:
+        print(lst)
+
+    for row in starting_board:
+        board_matrix.append([row])
+
+    G = nx.grid_2d_graph(6, 8)
+
+    edges = (
+        (node, (node[0] + dx, node[1] + dy))
+        for node in G.nodes
+        for dx, dy in [(1, 1), (-1, -1), (-1, 1), (1, -1)]
+        if 0 <= node[0] + dx < 6 and 0 <= node[1] + dy < 8
+    )
+    G.add_edges_from(edges)
+    
+    return G
+
 starting_board = [
     "SRETNU",
     "LPACEO",
@@ -11,32 +36,15 @@ starting_board = [
     "ASNTRB",
 ]
 
-board_matrix = []
+word_board = create_word_graph(starting_board)
 
-for row in starting_board:
-    board_matrix.append([row])
 
-for lst in board_matrix:
-    print(lst)
 
-for row in starting_board:
-    board_matrix.append([row])
-
-G = nx.grid_2d_graph(6, 8)
-
-edges = (
-    (node, (node[0] + dx, node[1] + dy))
-    for node in G.nodes
-    for dx, dy in [(1, 1), (-1, -1), (-1, 1), (1, -1)]
-    if 0 <= node[0] + dx < 6 and 0 <= node[1] + dy < 8
-)
-G.add_edges_from(edges)
-
-print(G.edges)
-print(G.number_of_edges())
+print(word_board.edges)
+print(word_board.number_of_edges())
 
 # Find hamiltonian paths from spanning trees
 paths = []
-for tree in nx.all_simple_paths(G, (0, 0), (5, 7)):
+for tree in nx.all_simple_paths(word_board, (0, 0), (5, 7)):
     paths.append(tree)
     print(len(paths))
--- train.py
+++ train.py
@@ -5,27 +5,28 @@ import numpy as np
 def abt(letter):
     return ord(letter) - 97
 
+def train_most_probable_matrix(length = 18):
+    words = dict()
+    zeProb = 0
+    with open("300k.txt", "r") as file:
+        for line in file.readlines():
+            word, freq = tuple(line.split("\t"))
+            words[word] = int(freq)
+            if len(word) > 1 and word[0] == "z" and word[1] == "e":
+                zeProb += int(freq)
+    print(zeProb)
+    matrix = np.zeros((27, 27, length), np.float64)
 
-words = dict()
-zeProb = 0
-with open("300k.txt", "r") as file:
-    for line in file.readlines():
-        word, freq = tuple(line.split("\t"))
-        words[word] = int(freq)
-        if len(word) > 1 and word[0] == "z" and word[1] == "e":
-            zeProb += int(freq)
-print(zeProb)
-matrix = np.zeros((27, 27, 18), np.float64)
+    for word in words.keys():
+        if len(word) <= length:
+            if len(word) > 1 and word[0] == "z" and word[1] == "e":
+                zeProb -= words[word]
+            for n, next_letter in enumerate(word[1:]):
+                curr_letter = word[n]
+                matrix[abt(curr_letter)][abt(next_letter)][n] += words[word]
+            matrix[abt(word[len(word) - 1])][26][n + 1] += words[word]
+    return matrix
 
-for word in words.keys():
-    if len(word) <= 18:
-        if len(word) > 1 and word[0] == "z" and word[1] == "e":
-            zeProb -= words[word]
-        for n, next_letter in enumerate(word[1:]):
-            curr_letter = word[n]
-            matrix[abt(curr_letter)][abt(next_letter)][n] += words[word]
-        matrix[abt(word[len(word) - 1])][26][n + 1] += words[word]
-
+matrix = train_most_probable_matrix()
 print(matrix[0][0][0])
-print(zeProb)
 print(matrix[25][4][0])
