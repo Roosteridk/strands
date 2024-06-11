7d6a431cae42aa0d5900bb1b9006ca0e418e1d3b
Arda <roosterdogan@gmail.com>
2024-06-11 17:21:02 -0400

    added prob matrix caching, string prob function

--- strands.py
+++ strands.py
@@ -1,16 +1,11 @@
 import networkx as nx
 import train
+import numpy as np
 
 
 def create_word_graph(matrix_to_board):
 
     board_matrix = []
-
-    for row in starting_board:
-        board_matrix.append([row])
-
-    for lst in board_matrix:
-        print(lst)
 
     for row in starting_board:
         board_matrix.append([row])
@@ -28,20 +23,6 @@ def create_word_graph(matrix_to_board):
     return G
 
 
-# def isLegal(value, maxVal):
-#     return value < maxVal and value >= 0
-
-# def find_most_probable_path(matrix, board, startRow, startCol):
-#     directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
-#     in_progress_heap = [(startRow,startCol)]
-#     final_heap = []
-#     row = startRow
-#     col = startCol
-#     while len(in_progress_heap) > 0:
-#         for dir in directions:
-#             if isLegal(row+dir[0],8) and isLegal(col+dir[1],6):
-
-
 starting_board = [
     "SRETNU",
     "LPACEO",
@@ -55,11 +36,33 @@ starting_board = [
 
 word_board = create_word_graph(starting_board)
 
+# def isLegal(value, maxVal):
+#     return value < maxVal and value >= 0
+
+# def find_most_probable_path(matrix, board, startRow, startCol):
+#     directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+#     in_progress_heap = [(startRow,startCol)]
+#     final_heap = []
+#     row = startRow
+#     col = startCol
+#     while len(in_progress_heap) > 0:
+#         for dir in directions:
+#             if isLegal(row+dir[0],8) and isLegal(col+dir[1],6):
+
 # matrix = train.train_most_probable_matrix()
 # for row in range(len(starting_board)):
 #     for col in range(len(starting_board[row])):
 #         currHeap = find_most_probable_path(matrix, starting_board, row, col)
 
 
-print(word_board.edges)
-print(word_board.number_of_edges())
+def get_prob_matrix():
+    try:
+        matrix = np.load("trained_matrix.npy")
+    except IOError:
+        matrix = train.train_most_probable_matrix()
+        np.save("trained_matrix", matrix)
+    finally:
+        return matrix
+
+
+print(train.find_string_prob("test", get_prob_matrix()))
--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 import numpy as np
-import math
 
 
-# Maps letter into number between 0 - 25
+# Maps letter onto number between 0 - 25
 def abt(letter):
     return ord(letter) - 97
 
@@ -21,36 +20,29 @@ def train_most_probable_matrix(length=18):
                 curr_letter = word[n]
                 matrix[n][abt(curr_letter)][abt(next_letter)] += words[word]
             matrix[n + 1][abt(word[len(word) - 1])][26] += words[word]
+
+    # Turn matrix values to probabilities
     for n in range(17):
         for curr in range(26):
-            log_freqs = np.log(matrix[n][curr])
-            print(log_freqs)
+            log_freqs = np.log(matrix[n][curr])  # Note: log(0) gives warning.
+            # print(log_freqs)
             exp_vector = np.exp(log_freqs)
-            print(exp_vector)
+            # print(exp_vector)
             prob_vector = np.divide((exp_vector), np.sum(exp_vector))
-            print(prob_vector)
-            print(np.sum(prob_vector))
-            quit()
+            # print(prob_vector)
+            # print(np.sum(prob_vector))
+            matrix[n][curr] = prob_vector
     return matrix
-
-
-matrix = train_most_probable_matrix()
-
-# Turn matrix values to probabilities
-# for n in range(17):
-#     for curr in range(26):
-#         log_freqs = np.log(matrix[n][curr])
-#         print(log_freqs)
-#         exp_vector = np.exp(log_freqs)
-#         print(exp_vector)
-#         prob_vector = np.divide((exp_vector), np.sum(exp_vector))
-#         print(prob_vector)
-#         print(np.sum(prob_vector))
-#         quit()
 
 
 # Might be better to continually count as we iterate
 # Raid->Raide->Raider, rather than continually
 # check Raid, Raide, Raider
-def find_string_prob(word):
-    return
+def find_string_prob(word, matrix):
+    if not (3 < len(word) <= 18):
+        raise Exception("Word must be longer than 3 characters and shorter than 19")
+    prob = 1
+    for n, next_letter in enumerate(word[1:]):
+        curr_letter = word[n]
+        prob *= matrix[n][abt(curr_letter)][abt(next_letter)]
+    return prob
